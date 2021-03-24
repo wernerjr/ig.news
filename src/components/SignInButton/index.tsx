9f4cf3030324d8d8c3700fb843d9cbe58c1e6132
Werner Gehrke Junior <werner.junior@outlook.com>
2021-03-24 20:10:41 -0300

    autenticação com next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [ session ] = useSession();
   
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}type="button">
+  return session ? (
+    <button 
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signOut()}
+
+    >
       <FaGithub color="#04d361" />
-      Werner Gehrke Junior
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (    
-    <button className={styles.signInButton}type="button">
+    <button 
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>)
