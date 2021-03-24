9ceb3dbfbcd6d654df934bec9a70360af3647691
Werner Gehrke Junior <werner.junior@outlook.com>
2021-03-23 22:57:23 -0300

    Conexão com o meio de pagamento stripes e SSG;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetStaticProps } from 'next'
 import styles from './home.module.scss';
 import Head from 'next/head'
-import { SubscribeButton } from '../Components/SubscribeButton';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -14,13 +23,32 @@ export default function Home() {
         <h1>News about the <span>React</span> world.</h1>
         <p>
           Get Access to all the publications <br />
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId} />
       </section>
 
       <img src="/images/avatar.svg" alt="Girl Coding"/>
     </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async() => {
+  const price = await stripe.prices.retrieve(process.env.STRIPE_PRICE_KEY)
+  
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
