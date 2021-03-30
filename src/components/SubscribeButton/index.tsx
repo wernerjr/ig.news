e3f23cfab3160895b96566a5d6201c0584f961b1
Werner Gehrke Junior <werner.junior@outlook.com>
2021-03-29 22:45:15 -0300

    Pagina de post e de preview validando a assinatura

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,18 +1,22 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/dist/client/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
-interface SubscribeButtonProps {
-  priceId: string;
-}
-
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+export function SubscribeButton() {
   const [ session ] = useSession();
-  
+  const router = useRouter();
+
   async function handleSubscribe() {
     if(!session){
       signIn('github');
+      return;
+    }
+
+    // @ts-ignore
+    if(session?.activeSubscription){
+      router.push('/posts');
       return;
     }
 
